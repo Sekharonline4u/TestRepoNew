a869fdc72fc816b43ff0754ce1bfa93010f3a471
sekharonline4u <sekharonline4u@gmail.com>
2020-12-17 23:45:05 +0530

    Second Commit

---
+++ UseCalcService/UseCalcService/NewFile.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UseCalcService
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewFile : ContentPage
+    {
+        public NewFile()
+        {
+            InitializeComponent();
+        }
+    }
+}
